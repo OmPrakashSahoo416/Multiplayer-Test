7a133636882c7051bf68ccc2b613d8d9ae75c55f
Om Prakash Sahoo <omprakashsahoo1234@gmail.com>
2021-11-27 20:10:32 +0530

    Added particle effects for hit effect and minor changes in level design.

--- Assets/Scripts/BulletProperties.cs
+++ Assets/Scripts/BulletProperties.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class BulletProperties : MonoBehaviour
 {
     [SerializeField] int count = 0;
+    [SerializeField] ParticleSystem bulletHitEffect;
+
+    private void Start()
+    {
+        bulletHitEffect = gameObject.GetComponentInChildren<ParticleSystem>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         count++;
-        if (count == 5)
+        if (count == 4)
         {
+            bulletHitEffect.Play();
             Destroy(gameObject);
             count = 0;
         }
